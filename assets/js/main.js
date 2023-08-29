d25b1312254544a4cf41080ceaf9753b4de379e6
Sams <goodlifesam@gmail.com>
2023-08-29 08:40:58 -0700

    added validate pawn and validate kind

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,3 @@
 This file is for linking and responsiveness purposes only. 
 NO CHESS LOGIC HERE OTHER THAN INITIALIZATION CALLS.
 */
-
-import Game from "./chess.js"
-
-const game = new Game
-game.startGame()
-
-console.log("hello from main.js")
